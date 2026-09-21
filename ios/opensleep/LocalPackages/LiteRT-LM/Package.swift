99fe4beced7ac93eed30645e6ae222efdf2362dd
Timmy Qian <164847362+timmyy123@users.noreply.github.com>
2026-09-21 19:09:04 +1000

    chore: bump app version to 1.0.4 and update LiteRT-LM dependency to v0.17.1

--- ios/opensleep/LocalPackages/LiteRT-LM/Package.swift
+++ ios/opensleep/LocalPackages/LiteRT-LM/Package.swift
@@ -32,14 +32,14 @@ let package = Package(
     .binaryTarget(
       name: "CLiteRTLM",
       url:
-        "https://github.com/google-ai-edge/LiteRT-LM/releases/download/v0.17.0/CLiteRTLM.xcframework.zip",
+        "https://github.com/google-ai-edge/LiteRT-LM/releases/download/v0.17.1/CLiteRTLM.xcframework.zip",
       checksum: "c94fc12aa0403cb47208e419cc3bfe258214ea17035f7a63c16de536869f2186"
     ),
     // The Prebuilt Binary Target for Mac
     .binaryTarget(
       name: "CLiteRTLM_mac",
       url:
-        "https://github.com/google-ai-edge/LiteRT-LM/releases/download/v0.17.0/CLiteRTLM_mac.xcframework.zip",
+        "https://github.com/google-ai-edge/LiteRT-LM/releases/download/v0.17.1/CLiteRTLM_mac.xcframework.zip",
       checksum: "83efd536485c9d58fcd7fb7d4556ddb16ca46bb775b0449d08d9825c6836c1a4"
     ),
     // The Swift Wrapper Target
